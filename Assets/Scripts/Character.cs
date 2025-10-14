61ab1a263abb8c172869e2cda6c2a2a6e4b8e24f
MalineeSunrise <malineebukong@gmail.com>
2025-10-14 16:59:24 +0700

    Lab8

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private int hearlth;
+    public int Health
+    {
+        get { return hearlth; }
+        set { hearlth = (value < 0) ? 0 : value; }
+    }
+
+    protected Animator anim;
+    protected Rigidbody2D rb;
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Debug.Log($"{this.name} took damage {damage} Current hearlth {Health}");
+
+        IsDead();
+    }
+
+    public bool IsDead()
+    {
+        if (Health <= 0)
+        {
+            Destroy(this.gameObject);
+            return true;
+        }
+        else { return false; }
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
